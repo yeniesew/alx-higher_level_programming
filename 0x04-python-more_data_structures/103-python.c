968c863c584491926f208b9963d7153c11a97960
yeniesew <yebrielij@gmail.com>
2023-03-15 02:25:30 -0700

    updated

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,7 +2,6 @@
 #include <Python.h>
 /**
  * print_python_bytes - Prints bytes information
- *
  * @p: Python Object
  * Return: no return
  */
@@ -10,21 +9,27 @@ void print_python_bytes(PyObject *p)
 {
 	char *string;
 	long int size, i, limit;
+
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+
 	size = ((PyVarObject *)(p))->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
+
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", string);
+
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
+
 	printf("  first %ld bytes:", limit);
+
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
 			printf(" %02x", string[i]);
@@ -35,7 +40,6 @@ void print_python_bytes(PyObject *p)
 
 /**
  * print_python_list - Prints list information
- *
  * @p: Python Object
  * Return: no return
  */
